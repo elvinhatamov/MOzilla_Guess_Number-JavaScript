021f9d11104ad3ad43d3982c8d52d334cca725aa
Elvin Hatamov <101150598@georgebrown.ca>
2022-04-07 01:18:49 -0400

    first commit

---
+++ number.js
@@ -0,0 +1,19 @@
+let randomNumber = Math.floor(Math.random()*100) +1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessFiled = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+
+function checkGUess() {
+ alert('I am a placeholder');
+
+}
+
+checkGUess();
